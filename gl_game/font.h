77f4fe8e4ec17b4f66a599b000ca406217cdc1f0
Andrew Holzer <megabyte770@gmail.com>
2015-12-13 09:41:08 -0800

    added font_init, render_text and display functions.

--- gl_game/font.h
+++ gl_game/font.h
@@ -4,4 +4,102 @@
 #include <ft2build.h>
 #include FT_FREETYPE_H
 
+FT_Library ft;
+FT_Face face;
+GLuint tex;
+
+GLuint font_init() {
+	if (FT_Init_FreeType(&ft)) {
+		fprintf(stderr, "Could not init freetype library\n");
+		return 1;
+	}
+
+	if (FT_New_Face(ft, "Consolas.ttf", 0, &face)) {
+		fprintf(stderr, "Could not open font\n");
+		return 1;
+	}
+
+	FT_Set_Pixel_Sizes(face, 0, 12);
+
+	if (FT_Load_Char(face, 'X', FT_LOAD_RENDER)) {
+		fprintf(stderr, "Could not load character 'X'\n");
+		return 1;
+	}
+
+	glActiveTexture(GL_TEXTURE0);
+	glGenTextures(1, &tex);
+	glBindTexture(GL_TEXTURE_2D, tex);
+	glUniform1i(uniform_tex, 0);
+
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+	glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
+
+	GLuint vbo;
+	glGenBuffers(1, &vbo);
+	glEnableVertexAttribArray(attribute_coord);
+	glBindBuffer(GL_ARRAY_BUFFER, vbo);
+	glVertexAttribPointer(attribute_coord, 4, GL_FLOAT, GL_FALSE, 0, 0);
+}
+
+void render_text(const char *text, float x, float y, float sx, float sy) {
+	const char *p;
+
+	for (p = text; *p; p++) {
+		if (FT_Load_Char(face, *p, FT_LOAD_RENDER))
+			continue;
+
+		glTexImage2D(
+			GL_TEXTURE_2D,
+			0,
+			GL_RED,
+			g->bitmap.width,
+			g->bitmap.rows,
+			0,
+			GL_RED,
+			GL_UNSIGNED_BYTE,
+			g->bitmap.buffer
+			);
+
+		float x2 = x + g->bitmap_left * sx;
+		float y2 = -y - g->bitmap_top * sy;
+		float w = g->bitmap.width * sx;
+		float h = g->bitmap.rows * sy;
+
+		GLfloat box[4][4] = {
+			{ x2,     -y2    , 0, 0 },
+			{ x2 + w, -y2    , 1, 0 },
+			{ x2,     -y2 - h, 0, 1 },
+			{ x2 + w, -y2 - h, 1, 1 },
+		};
+
+		glBufferData(GL_ARRAY_BUFFER, sizeof box, box, GL_DYNAMIC_DRAW);
+		glDrawArrays(GL_TRIANGLE_STRIP, 0, 4);
+
+		x += (g->advance.x >> 6) * sx;
+		y += (g->advance.y >> 6) * sy;
+	}
+}
+
+void display() {
+	glClearColor(1, 1, 1, 1);
+	glClear(GL_COLOR_BUFFER_BIT);
+
+	GLfloat black[4] = { 0, 0, 0, 1 };
+	glUniform4fv(uniform_color, 1, black);
+
+	float sx = 2.0 / glutGet(GLUT_WINDOW_WIDTH);
+	float sy = 2.0 / glutGet(GLUT_WINDOW_HEIGHT);
+
+	render_text("The Quick Brown Fox Jumps Over The Lazy Dog",
+		-1 + 8 * sx, 1 - 50 * sy, sx, sy);
+	render_text("The Misaligned Fox Jumps Over The Lazy Dog",
+		-1 + 8.5 * sx, 1 - 100.5 * sy, sx, sy);
+
+	glutSwapBuffers();
+}
+
 #endif
